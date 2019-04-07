4820535b35d7f5617048fa28e94a31464f71c6b2
John <grosabi.dev@gmail.com>
2019-04-07 01:29:39 -0400

    Version update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,15 +2,15 @@ var store = [{
         "title": "Test",
         "excerpt":"A banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called “plantains”, distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/test/",
+        "url": "https://gr0sabi.github.io/test/",
         "teaser":null},{
         "title": "Test2",
         "excerpt":"An apple is a sweet, edible fruit produced by an apple tree. Apple trees are cultivated worldwide, and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/test2/",
+        "url": "https://gr0sabi.github.io/test2/",
         "teaser":null},{
         "title": "Test3",
         "excerpt":"Kiwifruit (often abbreviated as kiwi), or Chinese gooseberry is the edible berry of several species of woody vines in the genus Actinidia. The most common cultivar group of kiwifruit is oval, about the size of a large hen’s egg (5–8 cm (2.0–3.1 in) in length and 4.5–5.5 cm (1.8–2.2 in)...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/test3/",
+        "url": "https://gr0sabi.github.io/test3/",
         "teaser":null}]
