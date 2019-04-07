39f1c7c516c406aa6dee0e85fcb7071560d30aa6
John <grosabi.dev@gmail.com>
2019-04-07 14:21:18 -0400

    Adding Test Information in Blog Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,6 @@
 var store = [{
-        "title": "A Simple Test Post",
-        "excerpt":"Hello Wanderer! This is definitely a test post. It will be removed in the future once I am happy with my blog configuration. Lorem Ipsum Gangsta Text Generator for those interested where I got the below: Pot ipsum shizzlin dizzle Pot ipsum shizzlin dizzle sit mah nizzle, gangsta adipiscing elit....","categories": ["blog"],
-        "tags": ["testing","jekyll"],
-        "url": "https://gr0sabi.github.io/blog/a-simple-test-post/",
-        "teaser":null},{
         "title": "Welcome!",
         "excerpt":"Hello World! This is my first Jekyll post hosted by Github Pages. The theme I use is Minimal Mistakes, by Michael Rose. Thanks for stopping by! Testing Code Blocks: x = \"Coding while using VS Code with an absurd amount of \" y = \"customizations and extensions is fun.\" print...","categories": ["blog"],
-        "tags": ["github","jekyll"],
+        "tags": ["github","jekyll","testing"],
         "url": "https://gr0sabi.github.io/blog/my-first-jekyll-post/",
         "teaser":null}]
