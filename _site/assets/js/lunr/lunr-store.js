999b775551e5b7e9e97f7f333531832b6a690adc
John <grosabi.dev@gmail.com>
2019-04-07 09:51:21 -0400

    Test Post Testing

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,9 +8,4 @@ var store = [{
         "excerpt":"An apple is a sweet, edible fruit produced by an apple tree. Apple trees are cultivated worldwide, and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands...","categories": [],
         "tags": [],
         "url": "https://gr0sabi.github.io/test2/",
-        "teaser":null},{
-        "title": "Test3",
-        "excerpt":"Kiwifruit (often abbreviated as kiwi), or Chinese gooseberry is the edible berry of several species of woody vines in the genus Actinidia. The most common cultivar group of kiwifruit is oval, about the size of a large hen’s egg (5–8 cm (2.0–3.1 in) in length and 4.5–5.5 cm (1.8–2.2 in)...","categories": [],
-        "tags": [],
-        "url": "https://gr0sabi.github.io/test3/",
         "teaser":null}]
