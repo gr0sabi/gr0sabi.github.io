c089a00b8502a40a63f2f6e728899e07e9e36629
John <grosabi.dev@gmail.com>
2020-02-09 11:41:05 -0500

    Blog style
    Major changes to blog style (colors/layout)

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,4 @@ var store = [{
         "excerpt":"Purpose I wrote this post to give back some of the insights I’ve discovered on my journey to becoming an OSCP. It will cover personal insights, resources, and best practices. There is a lot of great information available on the Internet so I will make an honest effort not to...","categories": ["security"],
         "tags": ["oscp","hacking","penetration testing","offensive security","certification"],
         "url": "https://gr0sabi.github.io/security/oscp-insights-best-practices-resources/",
-        "teaser":null}]
+        "teaser":"https://gr0sabi.github.io/assets/images/oscp-post/offsec-oscp.png"}]
