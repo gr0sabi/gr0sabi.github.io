33a2c52fb162939c26ed04e5dbcf1e08eb3832ac
John <grosabi.dev@gmail.com>
2020-05-02 13:47:07 -0400

    openadmin post fix

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": ["cissp","certification","study advice","best practices"],
         "url": "https://gr0sabi.github.io/certification/cissp-after-action-report/",
         "teaser": "https://gr0sabi.github.io/assets/images/cissp-post/CISSP.png"
+      },{
+        "title": "OpenAdmin Write-Up: HackTheBox",
+        "excerpt":"TL;DR OpenAdmin was a fun, easy machine with an interesting internal web application. With only two open ports, SSH (22) and HTTP (80) the attack surface is minimal. Web enumeration revealed a few web applications, one being an outdated OpenNetAdmin IP Address Management (IPAM) system (v18.1.1), which allowed RCE as...","categories": ["hackthebox"],
+        "tags": ["hackthebox","hacking","penetration testing","write-ups"],
+        "url": "https://gr0sabi.github.io/hackthebox/openadmin-write-up-hackthebox/",
+        "teaser": "https://gr0sabi.github.io/%5Cassets%5Cimages%5Chtb%5Copenadmin%5Cbanner-openadmin.png"
       }]
